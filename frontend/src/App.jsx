91ec8b43bdfc5b9870739f8612993e623d7d1a77
turbocrox <adityapundir502@gmail.com>
2025-07-28 19:20:45 +0530

    router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,36 @@
 import React from "react"
-import { BrowserRouter as  Router,Routes,Route } from "react-router-dom"
-import {Toaster} from "react-hot-toast"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Toaster } from "react-hot-toast"
 
-import Login from  "./pages/Auth/Login";
-import SignUp  from "./pages/Auth/SignUp";
-import LandingPage from  "./pages/LandingPage";
-import Dashboard from "./pages/Home/Dashboard";
-import InterviewPrep from  "./pages/InterviewPrep/InterviewPrep"
-
+import Login from "./pages/Auth/Login"
+import SignUp from "./pages/Auth/SignUp"
+import LandingPage from "./pages/LandingPage"
+import Dashboard from "./pages/Home/Dashboard"
+import InterviewPrep from "./pages/InterviewPrep/InterviewPrep"
 
 function App() {
-  
-
   return (
     <>
-    <div>
-      <Router>
-          <Router>
-             <Route path="/" element={<LandingPage/>}/>
-              <Route path="/login" element={<Login/>}/>
-               <Route path="/signup" element={<SignUp/>}/>
-                <Route path="/dashboard" element={<Dashboard/>}/>
-                 <Route path="/interview-prep/:sessionId" element={<InterviewPrep/>}/>
-          </Router>
-      </Router>
-    </div>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/interview-prep/:sessionId" element={<InterviewPrep />} />
+          </Routes>
+        </Router>
+
+        <Toaster
+          toastOptions={{
+            className: "",
+            style: {
+              fontSize: "13px",
+            },
+          }}
+        />
+      </div>
     </>
   )
 }
