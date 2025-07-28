aa0a72abe615fd7f6c4095db8e9cc55736d8b46a
turbocrox <adityapundir502@gmail.com>
2025-07-28 21:39:09 +0530

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Toaster } from "react-hot-toast"
 
-import Login from "./pages/Auth/Login"
-import SignUp from "./pages/Auth/SignUp"
 import LandingPage from "./pages/LandingPage"
 import Dashboard from "./pages/Home/Dashboard"
 import InterviewPrep from "./pages/InterviewPrep/InterviewPrep"
@@ -15,8 +13,7 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
+           
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/interview-prep/:sessionId" element={<InterviewPrep />} />
           </Routes>
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import Login from "./Auth/Login"
+import SignUp from "./Auth/SignUp"
 const LandingPage = () => {
   return (
     <div> <h1> hello </h1></div>
